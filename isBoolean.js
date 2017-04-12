55f52e64cda93937c3d9b9993d53579704b2fbb3
José Ángel <barrera@estudiodepublicidad.com>
2017-04-12 20:26:38 +0200

    The commit of exercises os week one

---
+++ isBoolean.js
@@ -0,0 +1,23 @@
+// isBoolean
+
+// Define a function called ​isBoolean​ that receives a value
+// and return true if the value received is a boolean
+
+// isBoolean(3) // false
+// isBoolean("true") // false
+// isBoolean(true) // true
+
+
+
+function isBoolean(value){
+	n = typeof(value)
+		if (n === 'boolean') {
+			return true
+							}
+		else { return false
+		}
+}
+
+
+isBoolean('sdgf')
+
